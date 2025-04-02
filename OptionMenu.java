c7ada9b234e77271ce8e2d7a644b7ad628f3de01
deekshitha-puppala <puppaladeekshitha2@gmail.com>
2025-04-02 23:55:01 +0530

    Add files via upload
    The Automated Teller Machine (ATM) program is a simple console-based application written in Java. It allows users to interact with the ATM and perform common banking operations: Withdraw: Users can withdraw money from their account, provided they have a sufficient balance.

---
+++ OptionMenu.java
@@ -0,0 +1,140 @@
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.*;
+import java.util.Map.Entry;
+
+public class OptionMenu extends Account {
+    Scanner menuInput = new Scanner(System.in);
+    DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.00");
+
+    HashMap<Integer, Integer> data = new HashMap<>();
+
+    // Customer number and Pin number
+    public void getLogin() throws IOException {
+        int x = 1;
+
+        do {
+            try {
+                data.put(9876543, 9876);
+                data.put(898989898, 1942);
+
+                System.out.println("Welcome to ATM Project!");
+
+                System.out.print("Enter your Customer Number: ");
+                setCustomerNumber(menuInput.nextInt());
+
+                System.out.print("Enter your Pin Number: ");
+                setPinNumber(menuInput.nextInt());
+            } catch (Exception e) {
+                System.out.println("\nInvalid character(s). Only numbers.\n");
+                x = 2;
+            }
+
+            for (Entry<Integer, Integer> entry : data.entrySet()) {
+                if (entry.getKey() == getCustomerNumber() && entry.getValue() == getPinNumber()) {
+                    getAccountType();
+                    return;
+                }
+            }
+
+            System.out.println("\nWrong Customer Number or Pin Number.\n");
+
+        } while (x == 1);
+    }
+
+    // Display the account type
+    public void getAccountType() {
+        System.out.println("Select the Account you want to Access: ");
+        System.out.println("Type 1 - Checking Account");
+        System.out.println("Type 2 - Saving Account");
+        System.out.println("Type 3 - Exit");
+        System.out.print("Choice: ");
+
+        int selection = menuInput.nextInt();
+
+        switch (selection) {
+            case 1:
+                getChecking();
+                break;
+            case 2:
+                getSaving();
+                break;
+            case 3:
+                System.out.println("Thank you for using this ATM.");
+                break;
+            default:
+                System.out.println("\nInvalid choice.\n");
+                getAccountType();
+        }
+    }
+
+    // Checking Account Options
+    public void getChecking() {
+        System.out.println("Checking Account:");
+        System.out.println("Type 1 - View Balance");
+        System.out.println("Type 2 - Withdraw Funds");
+        System.out.println("Type 3 - Deposit Funds");
+        System.out.println("Type 4 - Exit");
+        System.out.print("Choice: ");
+
+        int selection = menuInput.nextInt();
+
+        switch (selection) {
+            case 1:
+                System.out.println("Checking Account Balance: " + moneyFormat.format(getCheckingBalance()));
+                getAccountType();
+                break;
+            case 2:
+                getCheckingWithdrawInput();
+                getAccountType();
+                break;
+            case 3:
+                getCheckingDepositInput();
+                getAccountType();
+                break;
+            case 4:
+                System.out.println("Thank you for using this ATM.");
+                break;
+            default:
+                System.out.println("\nInvalid choice.\n");
+                getChecking();
+        }
+    }
+
+    // Saving Account Options
+    public void getSaving() {
+        System.out.println("Saving Account:");
+        System.out.println("Type 1 - View Balance");
+        System.out.println("Type 2 - Withdraw Funds");
+        System.out.println("Type 3 - Deposit Funds");
+        System.out.println("Type 4 - Exit");
+        System.out.print("Choice: ");
+        
+        int selection = menuInput.nextInt();
+
+        switch (selection) {
+            case 1:
+                System.out.println("Saving Account Balance: " + moneyFormat.format(getSavingBalance()));
+                getAccountType();
+                break;
+            case 2:
+                getSavingingWithdrawInput();
+                getAccountType();
+                break;
+            case 3:
+                getSavingingWithdrawInput();
+                getAccountType();
+                break;
+            case 4:
+                System.out.println("Thank you for using this ATM.");
+                break;
+            default:
+                System.out.println("\nInvalid choice.\n");
+               // getSaving();
+        }
+    }
+
+   
+      
+    }
